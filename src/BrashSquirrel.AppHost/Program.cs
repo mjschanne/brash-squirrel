422300bd5138b5fe5dba1a4c478a5e481ad47a14
MJ Schanne <mjschanne@microsoft.com>
2024-08-26 01:29:50 -0400

    Update modelVersion for availability

--- src/BrashSquirrel.AppHost/Program.cs
+++ src/BrashSquirrel.AppHost/Program.cs
@@ -4,10 +4,11 @@ var insights = builder.AddAzureApplicationInsights("appinsights");
 
 var openAi = builder.AddAzureOpenAI("AZURE-OPENAI-CONNSTR")
     .AddDeployment(
+        // https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#gpt-4-and-gpt-4-turbo-model-availability
         new AzureOpenAIDeployment(
             name: "chatmodel",
             modelName: "gpt-4o-mini",
-            modelVersion: "2024-07-18",
+            modelVersion: "2024-05-13",
             skuName: "GlobalStandard",
             skuCapacity: 1000));
 
